d20bbaa57534dcf763a9f1ba504bb19da24f0617
github username <github email address>
2020-01-21 13:49:12 -0500

    Show errors on the browser

--- streamsaver/app/controllers/users_controller.rb
+++ streamsaver/app/controllers/users_controller.rb
@@ -4,18 +4,23 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    redirect_to user_dashboard_path(@user)
+    @user = User.new(user_params)
+    if @user.valid? 
+      @user.save
+      redirect_to user_dashboard_path(@user)
+    else 
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_path
+    end 
   end
 
   def show
     @user = User.find(params[:id])
-    
   end
 
   private 
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :phone_number)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone_number)
   end 
 end
--- streamsaver/app/models/user.rb
+++ streamsaver/app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
     validates :first_name, presence: true, length: {maximum: 15}
     validates :last_name, presence: true, length: {maximum: 15}
     validates :password, presence: true, length: {in: 5..15 }
+    validates :password, confirmation: { case_sensitive: true}
     validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :phone_number, presence: true, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, 
-        message: "hone numbers must be in xxx-xxx-xxxx format."}
+        message: "must be in xxx-xxx-xxxx format."}
 
 
     def shows_by_provider
