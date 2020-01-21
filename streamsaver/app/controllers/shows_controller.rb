2e522d1f4182f73ca05f6016f42caaeea75dd748
github username <github email address>
2020-01-21 14:14:18 -0500

    new show

--- streamsaver/app/controllers/shows_controller.rb
+++ streamsaver/app/controllers/shows_controller.rb
@@ -1,10 +1,23 @@
 class ShowsController < ApplicationController
   def new
+    @show = Show.new
+    @providers = Provider.all
   end
 
   def create
+    @show = Show.create(show_params)
+    #handle errors
+    
+    redirect_to shows_path
   end
 
   def index
+    @shows = Show.all
+  end
+
+  private 
+  
+  def show_params
+    params.require(:show).permit(:name, :description, :image_url, :provider)
   end
 end
--- streamsaver/config/routes.rb
+++ streamsaver/config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   get 'providers/new', to: 'providers#new', as: 'new_provider'
   # post 'providers', to: 'providers#create', as: 'providers'
-  # get 'shows/new', to: 'shows#new', as: 'new_show'
-  # get 'shows/create', to: 'shows#create', as: 'create_show'
-  # get 'shows', to: 'shows#index', as: 'shows'
+  get 'shows/new', to: 'shows#new', as: 'new_show'
+  get 'shows/create', to: 'shows#create', as: 'create_show'
+  get 'shows', to: 'shows#index', as: 'shows'
   get 'users/new', to: 'users#new', as: 'new_user'
   post 'users', to: 'users#create'
   get 'users/:id', to: 'users#show', as: 'user_dashboard'
-  # post 'users/:id/favorites/create', as: 'new_favorite'
-  # get 'users/:id/favorites/plan', to: 'favorites#plan', as: 'user_plan' #indicate which months you want to watch each show
-  # post 'users/:id/favorites', to: 'favorites', as: 'create_plan'
+  #post 'users/:id/favorites/create', as: 'new_favorite'
+  get 'users/:id/favorites/new_plan', to: 'favorites#plan', as: 'user_plan' #indicate which months you want to watch each show
+  post 'users/:id/favorites', to: 'favorites', as: 'create_plan'
   
 
 end
