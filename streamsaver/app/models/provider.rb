675179a239fe9693955748b71ed6534c9e5cad71
github username <github email address>
2020-01-21 11:07:15 -0500

    Added validations

--- streamsaver/app/models/provider.rb
+++ streamsaver/app/models/provider.rb
@@ -1,3 +1,8 @@
 class Provider < ApplicationRecord
     has_many :shows
+
+    validates :name, presence: true, length: {maximum: 15}
+    validates :cost, presence: true, numericality: {greater_than: 0}
+    validates :url, presence: true, uniqueness: {case_sensitive: true}
+
 end
--- streamsaver/app/models/user.rb
+++ streamsaver/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
     has_many :favorites
     has_many :shows, through: :favorites
+
+    validates :first_name, presence: true, length: {maximum: 15}
+    validates :last_name, presence: true, length: {maximum: 15}
+    validates :password, presence: true, length: {in: 5..15 }
+    validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :phone_number, presence: true, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, 
+        message: "hone numbers must be in xxx-xxx-xxxx format."}
+
 end
