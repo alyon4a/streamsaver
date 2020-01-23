8aee5b97f6e2dc3981727281b091f58ed61f9d7d
github username <github email address>
2020-01-23 11:52:48 -0500

    Beautiful design for adding favorites

--- streamsaver/app/controllers/favorites_controller.rb
+++ streamsaver/app/controllers/favorites_controller.rb
@@ -3,18 +3,17 @@ class FavoritesController < ApplicationController
   #before_action :logged_in?
 
   def new 
-    @user = current_user
-    @shows = Show.all.select {|show| !@user.shows.include?(show)}
+    user = current_user
+    shows = Show.all.select {|show| !user.shows.include?(show)}
+
+    @shows_by_provider = Show.shows_by_provider(shows)
   end
   
   def create
-    
     new_favorites = []
-    show_ids = params[:user][:show_ids]
-    show_ids.each do |show_id|
-      if show_id != ""
+    
+    show_params.each do |show_id|
         new_favorites << Favorite.create(user_id: current_user.id, show_id: show_id)
-      end
     end
 
     @favorites = new_favorites
@@ -24,16 +23,28 @@ class FavoritesController < ApplicationController
   end
 
   def index
-    if current_user
-      @favorites = current_user.favorites
-    else 
-      redirect_to login_path
-    end 
+    # if current_user
+    #   @favorites = current_user.favorites
+    #   @shows_by_provider = Show.all_shows_by_provider
+    # else 
+    #   redirect_to login_path
+    # end 
   end
 
   def create_plan
     Favorite.parse_months(params[:fav_months])
     redirect_to user_dashboard_path
+  end
+
+  private
+
+  def show_params
+    params.keys.reduce([]) do |memo, param|
+      if param.start_with?('show')
+        memo << params[param].to_i
+      end
+      memo
+    end
   end
 
 end
