7a969dbf287d65b9e86ed04946f2fe38184dc29a
github username <github email address>
2020-01-24 10:08:12 -0500

    new login page with a video

--- streamsaver/config/application.rb
+++ streamsaver/config/application.rb
@@ -8,9 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Streamsaver
   class Application < Rails::Application
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
